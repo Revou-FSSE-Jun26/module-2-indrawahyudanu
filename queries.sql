5974f29494a84270ce44d70ad50d08dc6622104b
Indra Wahyu <indrawahyudanu@gmail.com>
2026-08-01 16:27:17 +0700

    Refactor SQL queries for better organization and clarity

--- queries.sql
+++ queries.sql
@@ -1,26 +1,14 @@
-SELECT 
-    p.product_id, 
-    p.product_name, 
-    p.price, 
-    p.stock, 
-    c.category_name 
-FROM products p
-JOIN categories c ON p.category_id = c.category_id;
 
--- 2. Mencari produk dengan harga di atas 100.000 (WHERE)
+--1. show table users, products, orders.
+select * from users; 
+select * from products;
+select * from orders;
+
+--2. show data price product more than 100000.
 SELECT * FROM products 
 WHERE price > 100000.00;
 
--- 3. Menampilkan 3 produk termurah (ORDER BY + LIMIT)
+--3. Display the 3 cheapest product 
 SELECT * FROM products 
 ORDER BY price ASC 
 LIMIT 3;
-
--- 4. Menampilkan riwayat pesanan user beserta total belanjaannya (JOIN)
-SELECT 
-    o.order_id, 
-    u.username, 
-    o.order_date, 
-    o.total_amount 
-FROM orders o
-JOIN users u ON o.user_id = u.user_id;
