3986ee596f8e823689cb8fe99eb11c6720337d1d
Indra Wahyu <indrawahyudanu@gmail.com>
2026-08-16 22:19:59 +0700

    Add role and password hash columns to user table in migration and model

--- models.py
+++ models.py
@@ -34,7 +34,9 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
+    role = db.Column(db.String(50), nullable=False, server_default='user')
     email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False, server_default="'passwordhas123'")
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def to_dict(self):
