9b6f0bfc044ab9831f483c8c33963521def27ed2
Indra Wahyu <indrawahyudanu@gmail.com>
2026-09-01 21:12:54 +0700

    feat: remove default values for database URI and JWT secret key in config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:1234567@localhost:5432/revoshop_db")
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key") 
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY") 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
