b96b13a7b94bbba67de6e2cb664c1bc5f971021f
Indra Wahyu <indrawahyudanu@gmail.com>
2026-09-05 11:36:59 +0700

    fix: update home route to use root path and remove url prefix

--- routes/routes.py
+++ routes/routes.py
@@ -2,10 +2,10 @@ from flask import Blueprint ,jsonify, request
 from werkzeug.security import generate_password_hash, check_password_hash
 from utils import db
 
-home_bp = Blueprint('home', __name__, url_prefix='/home')
+home_bp = Blueprint('home', __name__)
 
 
 # GET home route
-@home_bp.route('/new')
+@home_bp.route('/')
 def home():
     return jsonify({"message": "demo, status, oke "})
